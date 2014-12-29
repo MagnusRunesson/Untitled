f28bac41051ea5f12d8ba38ea5c85ed5d8ba1e81
Magnus Runesson <magnus@pokewhat.com>
2014-12-29 21:00:46 +0100

    Moved vasm for osx into the git repo Modified build scripts to use the local vasm

--- build/mess/build.sh
+++ build/mess/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ..
-~/Downloads/vasm/vasmm68k_mot -Fbin -o megadrive.bin megadrive.asm -m68000
+../bin/osx/vasmm68k_mot -Fbin -o megadrive.bin megadrive.asm -m68000
 
 cd mess
--- build/mess/buildanddebug.sh
+++ build/mess/buildanddebug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-~/Downloads/vasm/vasmm68k_mot -Fbin -o megadrive.bin megadrive.asm -m68000
+../bin/osx/vasmm68k_mot -Fbin -o megadrive.bin megadrive.asm -m68000
 
 cd mess
 ~/Downloads/mess0155-32bit/mess genesis -cart ../megadrive.bin -window -waitvsync -nofilter -debug -skip_gameinfo
