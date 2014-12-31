b8a79c639aaffb09b45b6b8c670796144a4205bf
Magnus Runesson <magnus@pokewhat.com>
2014-12-31 16:45:12 +0100

    Main loop avoid using d0 and d1 from now on since they will be scratch registers

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,6 @@
 main:
-	move.l		#0,d0					; d0 = scroll position for background layer
-	move.l		#0,d1					; d1 = scroll vertical
+	move.l		#0,d2					; d2 = x scroll position for background layer
+	move.l		#0,d3					; d3 = y scroll position for background layer
 
 	jsr			memGetUserBaseAddress	;
 	move.l		a0,a2					; a2 will be user mem from now on
@@ -24,14 +24,15 @@ main:
 
 	btst		#INPUT_RIGHT,d7
 	beq			.scroll_right
+
 	bra			.scroll_updown
 
 .scroll_left:
-	addq.w		#1,d0
+	addq.w		#1,d2
 	bra			.scroll_updown
 
 .scroll_right:
-	subq.w		#1,d0
+	subq.w		#1,d2
 	bra			.scroll_updown
 
 .scroll_updown:
@@ -44,17 +45,22 @@ main:
 	bra			.done
 
 .scroll_up:
-	subq.w		#1,d1
+	subq.w		#1,d3
 	bra			.done
 
 .scroll_down:
-	addq.w		#1,d1
+	addq.w		#1,d3
 	bra			.done
 
 .done:
 	jsr			rendWaitVSync
+
+	; Update scrolling position
+	move.l		d2,d0					; x position
+	move.l		d3,d1					; y position
 	jsr			rendSetScrollXY			; d0=x position, d1=y position
 
+	;
 	bra			.loop
 
 	nop
