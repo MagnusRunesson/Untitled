c2f17e9e5d27b38ab64f7f8a4566dbfb100c9465
Magnus Runesson <magnus@pokewhat.com>
2015-01-02 18:04:10 +0100

    Added include path for NDK 3.9

--- build/amiga/build.sh
+++ build/amiga/build.sh
@@ -1,2 +1,2 @@
-../../bin/osx/vasmm68k_mot -Fbin -o amiga.adf ../amiga.asm -m68000
+../../bin/osx/vasmm68k_mot -Fbin -o amiga.adf ../amiga.asm -m68000 -I"/Users/magnusrunesson/Projects/Amiga/NDK_3.9/Include/include_i"
 ../../bin/osx/makeimage amiga.adf
