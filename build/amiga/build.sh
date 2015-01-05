c5b3552cb7ae69ef3a1ff0a2f297b48534f1ebe1
Krister Jansson <krustur@gmail.com>
2015-01-05 23:56:44 +0100

    Re-added fs.uae-config file Deleted old and unused build.sh file

--- build/amiga/build.sh
+++
@@ -1,2 +0,0 @@
-../../bin/osx/vasmm68k_mot -Fbin -o amiga.adf ../amiga.asm -m68000 -I"/Users/magnusrunesson/Projects/Amiga/NDK_3.9/Include/include_i"
-../../bin/osx/makeimage amiga.adf
--- build/runadf.bat
+++ build/runadf.bat
@@ -1,3 +1,3 @@
 @echo off
 
-C:\Users\Krister\AppData\Local\fs-uae\fs-uae.exe cfg\untitledadf.fs-uae
+C:\Users\Krister\AppData\Local\fs-uae\fs-uae.exe uae\untitledadf.fs-uae
