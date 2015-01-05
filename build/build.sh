56041f76f0116383306ddea732fe8eeb46573b64
Magnus Runesson <magnus@pokewhat.com>
2015-01-05 08:38:15 +0100

    Updated OSX build script to build both Amiga and Mega Drive at once

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-cd ..
-../bin/osx/vasmm68k_mot -Fbin -o megadrive.bin megadrive.asm -m68000
 
-cd mess
+#
+# Build Mega Drive
+#
+../bin/osx/vasmm68k_mot -Fbin -o out/untitled_megadrive.bin megadrive.asm -m68000
+
+#
+# Build Amiga
+#
+../bin/osx/vasmm68k_mot -Fbin -o out/untitled.adf amiga.asm -m68000 -I"/Users/magnusrunesson/Projects/Amiga/NDK_3.9/Include/include_i"
+../bin/osx/makeimage out/untitled.adf
