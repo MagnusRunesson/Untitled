d8468866303817f3ce1019a4e2522277ed1fb819
Magnus Runesson <magnus@pokewhat.com>
2015-01-05 08:38:34 +0100

    Renamed the output file from the Mega Drive build

--- build/build.bat
+++ build/build.bat
@@ -3,4 +3,4 @@
 ..\bin\win32\vasmm68k_mot_win32 -Fbin -o out/untitled.adf amiga.asm -m68000 -I"F:/Amiga/Harddrives/Hdf My Amiga 1200/NewCode/NDK_3.9/Include/include_i"
 ..\bin\win32\makeimage out/untitled.adf
 
-..\bin\win32\vasmm68k_mot_win32 -Fbin -o out/megadrive.bin megadrive.asm -m68000
+..\bin\win32\vasmm68k_mot_win32 -Fbin -o out/untitled_megadrive.bin megadrive.asm -m68000
