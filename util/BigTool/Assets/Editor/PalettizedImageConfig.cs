42ffdbef76838800f1bfc9b9f8ab6d0186e7124b
Magnus Runesson <magnusrunesson@Magnuss-MacBook-Pro.local>
2015-01-13 13:12:32 +0100

    Changed format of Color Remap Table in config from string,string pairs to string,int pairs Added Import As Sprite setting for image

--- util/BigTool/Assets/Editor/PalettizedImageConfig.cs
+++ util/BigTool/Assets/Editor/PalettizedImageConfig.cs
@@ -5,11 +5,16 @@ using System.Collections.Generic;
 public class PalettizedImageConfig
 {
 	const string JSONKEY_COLORREMAPTABLE = "Color_Remap_Table";
+	const string JSONKEY_IMPORTASSPRITE = "Import_As_Sprite";
+
+	string m_fileName;
 
 	public Dictionary<int,int> m_colorRemapSourceToDest;
+	public bool m_importAsSprite;
 
 	public PalettizedImageConfig( string _path )
 	{
+		m_fileName = _path;
 		SetupDefaults();
 
 		// Verify that file exist
@@ -21,6 +26,7 @@ public class PalettizedImageConfig
 		var dict = MiniJSON.Json.Deserialize( jsonString ) as Dictionary<string,object>;
 
 		LoadColorMapTable( (Dictionary<string,object>)dict[ JSONKEY_COLORREMAPTABLE ]);
+		LoadBool( ref m_importAsSprite, JSONKEY_IMPORTASSPRITE, dict );
 
 		/*
 		Debug.Log("deserialized: " + dict.GetType());
@@ -33,18 +39,32 @@ public class PalettizedImageConfig
 
 		//return image;
 	}
-	
+
+	public void Save()
+	{
+		// Create a new settings ditionary
+		Dictionary<string,object> jsonDict = new Dictionary<string, object>();
+		jsonDict[ JSONKEY_COLORREMAPTABLE ] = m_colorRemapSourceToDest;
+		jsonDict[ JSONKEY_IMPORTASSPRITE ] = m_importAsSprite;
+
+		// Generate JSON string from settings dictionary
+		string jsonString = MiniJSON.Json.Serialize( jsonDict );
+
+		System.IO.File.WriteAllText( m_fileName, jsonString );
+	}
+
 	void SetupDefaults()
 	{
-		//
-		m_colorRemapSourceToDest = new Dictionary<int, int>();
-		
 		// Setup default mapping table
+		m_colorRemapSourceToDest = new Dictionary<int, int>();
 		int i;
 		for( i=0; i<16; i++ )
 		{
 			m_colorRemapSourceToDest[ i ] = i;
 		}
+
+		//
+		m_importAsSprite = false;
 	}
 
 	void LoadColorMapTable( Dictionary<string,object> _table )
@@ -57,12 +77,17 @@ public class PalettizedImageConfig
 			int value;
 			if( int.TryParse( kvp.Key, out key ))
 			{
-				if( int.TryParse( (string)kvp.Value, out value ))
-				{
-					Debug.Log ("Index " + key + " remap to " + value );
-					m_colorRemapSourceToDest[ value ] = key;
-				}
+				value = (int)(System.Int64)kvp.Value;
+				m_colorRemapSourceToDest[ value ] = key;
 			}
+		}
+	}
+
+	void LoadBool( ref bool _out, string _key, Dictionary<string,object> _json )
+	{
+		if( _json.ContainsKey( _key ))
+		{
+			_out = (bool)_json[ _key ];
 		}
 	}
 }
