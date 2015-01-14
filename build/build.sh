63b284117ebe83742f3cce3b9f3b6f2bd75b0c80
Magnus Runesson <magnusrunesson@Magnuss-MacBook-Pro.local>
2015-01-14 09:02:26 +0100

    Changed build script to make it easier to see what's going on.

--- build/build.sh
+++ build/build.sh
@@ -5,8 +5,8 @@
 #
 echo
 echo
-echo "=======> Building Mega Drive"
 echo
+echo "=======> Building Mega Drive"
 echo
 ../bin/osx/vasmm68k_mot -Fbin -o out/untitled_megadrive.bin megadrive.asm -m68000
 
@@ -15,8 +15,8 @@ echo
 #
 echo
 echo
-echo "=======> Building Amiga"
 echo
+echo "=======> Building Amiga"
 echo
 ../bin/osx/vasmm68k_mot -Fbin -o out/untitled.adf amigaadf.asm -m68000 -I"/Users/magnusrunesson/Projects/Amiga/NDK_3.9/Include/include_i" -I"/Users/magnusrunesson/Projects/md/Untitled/src/amiga_lib"
 ../bin/osx/makeimage out/untitled.adf
