a5bbeeadaf72b89b30719415bf24fa6176f20e00
Magnus Runesson <magnusrunesson@Magnuss-MacBook-Pro.local>
2015-01-19 09:10:13 +0100

    Added frame times to palettized image config

--- util/BigTool/Assets/Editor/PalettizedImageConfig.cs
+++ util/BigTool/Assets/Editor/PalettizedImageConfig.cs
@@ -4,14 +4,18 @@ using System.Collections.Generic;
 
 public class PalettizedImageConfig
 {
+	const int DEFAULT_FRAME_TIME = 6;		// 10 frames per second for animation when game is running at 60 FPS
+	
 	const string JSONKEY_COLORREMAPTABLE = "Color_Remap_Table";
 	const string JSONKEY_IMPORTASSPRITE = "Import_As_Sprite";
 	const string JSONKEY_SPRITENUMFRAMES = "Sprite_Num_Frames";
+	const string JSONKEY_FRAMETIMES = "Frame_Times";
 
 	string m_fileName;
 
 	public Dictionary<int,int> m_colorRemapSourceToDest;
 	public bool m_importAsSprite;
+	public Dictionary<int,int> m_frameTimes;
 
 	int m_spriteFrames;
 	int m_spriteWidth;
@@ -32,6 +36,7 @@ public class PalettizedImageConfig
 		var dict = MiniJSON.Json.Deserialize( jsonString ) as Dictionary<string,object>;
 
 		LoadColorMapTable( (Dictionary<string,object>)dict[ JSONKEY_COLORREMAPTABLE ]);
+		LoadFrameTimes( dict );
 		LoadBool( ref m_importAsSprite, JSONKEY_IMPORTASSPRITE, dict );
 		LoadInt( ref m_spriteFrames, JSONKEY_SPRITENUMFRAMES, dict );
 	}
@@ -43,6 +48,7 @@ public class PalettizedImageConfig
 		jsonDict[ JSONKEY_COLORREMAPTABLE ] = m_colorRemapSourceToDest;
 		jsonDict[ JSONKEY_IMPORTASSPRITE ] = m_importAsSprite;
 		jsonDict[ JSONKEY_SPRITENUMFRAMES ] = m_spriteFrames;
+		jsonDict[ JSONKEY_FRAMETIMES ] = m_frameTimes;
 
 		// Generate JSON string from settings dictionary
 		string jsonString = MiniJSON.Json.Serialize( jsonDict );
@@ -76,7 +82,22 @@ public class PalettizedImageConfig
 	{
 		return m_spriteFrames;
 	}
-	
+
+	public int GetFrameTime( int _frame )
+	{
+		// Return the given frame time of any
+		if( m_frameTimes.ContainsKey( _frame ))
+			return m_frameTimes[ _frame ];
+
+		// Return default if there haven't been a frame time specified for this frame
+		return DEFAULT_FRAME_TIME;
+	}
+
+	public void SetFrameTime( int _frame, int _time )
+	{
+		m_frameTimes[ _frame ] = _time;
+	}
+
 	void RefreshInternalThings()
 	{
 		m_spriteWidth = m_imageData.m_width / m_spriteFrames;
@@ -100,6 +121,7 @@ public class PalettizedImageConfig
 		//
 		m_importAsSprite = false;
 		m_spriteFrames = 1;
+		m_frameTimes = new Dictionary<int, int>();
 	}
 
 	void LoadColorMapTable( Dictionary<string,object> _table )
@@ -113,6 +135,26 @@ public class PalettizedImageConfig
 			{
 				value = (int)(System.Int64)kvp.Value;
 				m_colorRemapSourceToDest[ value ] = key;
+			}
+		}
+	}
+
+	void LoadFrameTimes( Dictionary<string,object> _json )
+	{
+		m_frameTimes = new Dictionary<int, int>();
+
+		// Bail out if there are no frame times in JSON
+		if( _json.ContainsKey( JSONKEY_FRAMETIMES ) == false )
+			return;
+
+		// Parse the table in the config file
+		Dictionary<string,object> frameTimesJson = (Dictionary<string,object>)_json[ JSONKEY_FRAMETIMES ];
+		foreach( KeyValuePair<string,object> kvp in frameTimesJson )
+		{
+			int key;
+			if( int.TryParse( kvp.Key, out key ))
+			{
+				m_frameTimes[ key ] = (int)(long)kvp.Value;
 			}
 		}
 	}
