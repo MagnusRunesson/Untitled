f7a4933f42860bcdf98002119425490299e83e39
Magnus Runesson <magnusrunesson@Magnuss-MacBook-Pro.local>
2015-01-19 13:07:02 +0100

    Renderer needs more memory

--- src/platform/megadrive/mem.asm
+++ src/platform/megadrive/mem.asm
@@ -2,7 +2,7 @@ platform_mem_size				= $2000
 platform_mem_start				= $00ff0000 + totalmem_size
 
 platform_renderer_start			= platform_mem_start
-platform_renderer_size			= $100
+platform_renderer_size			= $250
 
 VRAM_MapTiles_Start				= $0000
 VRAM_SpriteTiles_Start			= $bd80		; This one goes down when allocated, so it should be the same as another VRAM tag
