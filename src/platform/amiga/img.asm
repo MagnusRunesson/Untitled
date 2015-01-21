39a348442c7b3d73849d81da07b2772c6392e8b5
Krister Jansson <krustur@gmail.com>
2015-01-22 00:11:44 +0100

    Copied imgLoad code from mega drive to Amiga. Though removed calls to rend-functions.

--- src/platform/amiga/img.asm
+++ src/platform/amiga/img.asm
@@ -1,3 +1,24 @@
 ; a0 = Pointer to image data to load
 imgLoad:
+	move	#0,d0				; Since I don't know how to fetch a word and clear the top bits I do this first
+	move.w	(a0)+,d0			; Fetch file ID of the tile bank and load it
+	move	#0,d1				; Destination address in VRAM
+	move.l	a0,-(sp)
+	;jsr		rendLoadTileBank
+	move.l	(sp)+,a0
+
+	; Load map
+	move.w	(a0)+,d0			; Fetch file ID of the tile bank and load it
+	move	#0,d1				; #0 is the background layer
+	move.l	a0,-(sp)
+	;jsr		rendLoadTileMap
+	move.l	(sp)+,a0
+
+	; Load palette
+	move.w	(a0)+,d0			; Fetch file ID of the tile bank and load it
+	move	#0,d1				; #0 is the background palette
+	move.l	a0,-(sp)
+	;jsr		rendLoadPalette
+	move.l	(sp)+,a0
+
 	rts
