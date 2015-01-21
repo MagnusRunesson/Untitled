7c59171b2197f43b674402a90a6e648bb53b3822
Krister Jansson <krustur@gmail.com>
2015-01-22 00:04:29 +0100

    Moved mega drive includes above org to decrease the distance to PC

--- build/megadrive.asm
+++ build/megadrive.asm
@@ -11,11 +11,12 @@ _chunk_size		equ		128			; same as TD_SECTOR (size of sector on floppy)
 	include		"../src/platform/megadrive/file.asm"
 	include		"../src/platform/megadrive/img.asm"
 
+	include		"../src/incbin/untitled_splash_image.asm"
+	include		"../src/incbin/testtiles_image.asm"
+
 	org			$10000
 
 	include		"../src/incbin/data.asm"
 	include		"../src/incbin/files.asm"
-	include		"../src/incbin/untitled_splash_image.asm"
-	include		"../src/incbin/testtiles_image.asm"
 
 	org			$20000
