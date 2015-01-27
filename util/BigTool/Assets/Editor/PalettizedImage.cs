4f5bfe8db58a12dc4067f87cca7832e7a93bf443
Magnus Runesson <magnusrunesson@Magnuss-MacBook-Pro.local>
2015-01-27 09:07:02 +0100

    Added PNG support in the PalettizedImage class

--- util/BigTool/Assets/Editor/PalettizedImage.cs
+++ util/BigTool/Assets/Editor/PalettizedImage.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Hjg.Pngcs;
+using Hjg.Pngcs.Chunks;
 
 public class PalettizedImage
 {
@@ -12,6 +14,39 @@ public class PalettizedImage
 	public string m_fileName;
 	public PalettizedImageConfig m_config;
 	int m_coloursInPalette;
+
+	static public PalettizedImage LoadImage( string _path, PalettizedImageConfig _config )
+	{
+		string ext = System.IO.Path.GetExtension( _path ).ToLower();
+		if( ext.Equals( ".bmp" ))
+			return LoadBMP( _path, _config );
+
+		if( ext.Equals( ".png" ))
+			return LoadPNG( _path, _config );
+
+		return null;
+	}
+
+	static public PalettizedImage LoadPNG( string _path, PalettizedImageConfig _config )
+	{
+		PngReader pngr = FileHelper.CreatePngReader( _path );
+		if( !pngr.ImgInfo.Indexed )
+		{
+			Debug.LogException( new UnityException( "Image wasn't indexed" ));
+			return null;
+		}
+
+		PngChunkPLTE palette = pngr.GetMetadata().GetPLTE();
+
+		PalettizedImage image = new PalettizedImage( pngr.ImgInfo.Cols, pngr.ImgInfo.Rows, palette.GetNentries());
+		image.SetConfig( _config );
+		image.m_fileName = System.IO.Path.GetFileNameWithoutExtension( _path );
+
+		image.ReadPalette( palette );
+		image.ReadImage( pngr );
+
+		return image;
+	}
 
 	static public PalettizedImage LoadBMP( string _path, PalettizedImageConfig _config )
 	{
@@ -124,6 +159,85 @@ public class PalettizedImage
 		return true;
 	}
 
+	bool ReadPalette( PngChunkPLTE _palette )
+	{
+		int[] rgb = new int[ 4 ];
+		int c;
+		for( c=0; c<m_coloursInPalette; c++ )
+		{
+			int c2 = c;
+			if( m_config.m_colorRemapSourceToDest.ContainsKey( c ))
+				c2 = m_config.m_colorRemapSourceToDest[ c ];
+
+			_palette.GetEntryRgb( c2, rgb );
+			float fr = ((float)rgb[ 0 ]) / 255.0f;
+			float fg = ((float)rgb[ 1 ]) / 255.0f;
+			float fb = ((float)rgb[ 2 ]) / 255.0f;
+			//float fa = ((float)a) / 255.0f;
+			float fa = 1.0f;
+			
+			/*
+			if((c%16) == 0 )
+				fa = 0.0f;
+			else
+				fa = 1.0f;
+				*/
+			
+			Color col = new Color( fr, fg, fb, fa );
+			m_palette.Add( col );
+			m_colorUsed.Add( false );
+		}
+		
+		while( c < 16 )
+		{
+			m_palette.Add( new Color( 1.0f, 0.0f, 1.0f, 1.0f ));
+			m_colorUsed.Add( false );
+			c++;
+		}
+		
+		return true;
+	}
+	
+	bool ReadImage( PngReader _png )
+	{
+		/*
+		ImageLine line = pngr.ReadRowInt( y );
+		int[] scanline = line.Scanline;
+		
+		string l = "";
+		for( int x=0; x<scanline.Length; x++ )
+		{
+			int ci = scanline[ x ];
+			l += ci.ToString() + ",";
+		}
+		
+		Debug.Log ("line " + y + ": " + l );
+		*/
+
+
+		int x,y;
+		for( y=0; y<m_height; y++ )
+		{
+			ImageLine line = _png.ReadRowByte( y );
+			//byte[] lineBytes = _png.ReadRowByte( y );
+			byte[] lineBytes = line.ScanlineB;// GetScanlineInt();
+
+			for( x=0; x<m_width; x++ )
+			{
+				byte src_c = lineBytes[ x ];
+				byte remapped_c = src_c;
+				if( m_config.m_colorRemapSourceToDest.ContainsKey( (int)src_c ))
+					remapped_c = (byte)m_config.m_colorRemapSourceToDest[ src_c ];
+				m_colorUsed[ remapped_c ] = true;
+
+				int dst_i = (y*m_width)+x;
+				m_image[ dst_i ] = remapped_c;
+			}
+		}
+
+		return true;
+	}
+	
 	static int ReadInt( byte[] _array, int _offset )
 	{
 		int ret = _array[ _offset ] + (_array[ _offset+1 ] << 8) + (_array[ _offset+2 ]<<16) + (_array[ _offset+3 ]<<24);
