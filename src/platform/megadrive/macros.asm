2ba08a9a2093cfb766aed0c589426936e2561d2d
Magnus Runesson <magnus@pokewhat.com>
2015-01-29 19:20:43 +0100

    Added comments to the Mega Drive macros.asm

--- src/platform/megadrive/macros.asm
+++ src/platform/megadrive/macros.asm
@@ -1,8 +1,14 @@
+;
+; Will set the border color to index 0 in the palette. Might get modified so the color index is a parameter.
+;
 perf_start		MACRO
 	move.w		#$8700,($00C00004)
 	ENDM
 
 
+;
+; Will set the border color to index 1 in the palette. Might get modified so the color index is a parameter.
+;
 perf_stop		MACRO
 	move.w		#$8701,($00C00004)
 	ENDM
