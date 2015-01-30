848ec46380e2bec8d022214d0051185951f3dd9c
Krister Jansson <krustur@gmail.com>
2015-01-30 08:11:19 +0100

    Coded fileLoad function for Amiga

--- src/platform/amiga/file.asm
+++ src/platform/amiga/file.asm
@@ -1,16 +1,24 @@
 ;==============================================================================
 ;
-; Get the address to a file
+; Load file to specified memory address
 ;
 ; Input:
 ;	d0=file ID
+;	a0=address to load file to
 ;
 ; Output
-;	a0=address to file data
-;	d0=file size
+;	none
 ;
 ;==============================================================================
 fileLoad:
-	move.l		#0,a0
-	move.l		#0,d0
+	lea			FileIDMap(pc),a1
+	asl.l		#2,d0
+	add.l		d0,a1
+	moveq		#0,d0
+	moveq		#0,d1
+	move.w		(a1)+,d0
+	move.w		(a1),d1
+
+	bsr			trackdiskLoadBlock
+
 	rts
