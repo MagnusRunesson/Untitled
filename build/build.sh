6de15d42e5e1073f5b384307896e8f7e7c987b4b
Krister Jansson <krustur@gmail.com>
2015-01-30 19:16:30 +0100

    Added is_mega_drive and is_amiga symbols to build scripts

--- build/build.sh
+++ build/build.sh
@@ -8,7 +8,7 @@ echo
 echo
 echo "=======> Building Mega Drive"
 echo
-../bin/osx/vasmm68k_mot -Fbin -o out/untitled_megadrive.bin megadrive.asm -m68000
+../bin/osx/vasmm68k_mot -Fbin -o out/untitled_megadrive.bin megadrive.asm -Dis_mega_drive -m68000
 
 #
 # Build Amiga
@@ -18,7 +18,7 @@ echo
 echo
 echo "=======> Building Amiga"
 echo
-../bin/osx/vasmm68k_mot -Fbin -o out/untitled.adf amigaadf.asm -m68000 -I"/Users/magnusrunesson/Projects/Amiga/NDK_3.9/Include/include_i" -I"/Users/magnusrunesson/Projects/md/Untitled/src/amiga_lib"
+../bin/osx/vasmm68k_mot -Fbin -o out/untitled.adf amigaadf.asm -Dis_amiga -m68000 -I"/Users/magnusrunesson/Projects/Amiga/NDK_3.9/Include/include_i" -I"/Users/magnusrunesson/Projects/md/Untitled/src/amiga_lib"
 ../bin/osx/makeimage out/untitled.adf
 
 echo
