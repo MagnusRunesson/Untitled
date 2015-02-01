dae76ba39261b2983d22640a4d1f3b30e93ae8b3
Magnus Runesson <magnus@pokewhat.com>
2015-02-01 20:04:01 +0100

    Experimenting with objects in Tiled

---
+++ datawork/Objects (editor only).tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Objects (editor only)" tilewidth="16" tileheight="16">
+ <tile id="0">
+  <image width="16" height="16" source="editor icons/icon_potion.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="editor icons/icon_signpost.png"/>
+ </tile>
+</tileset>
