927a4b54a4b16637587375bdee2aa1f6b64de635
Krister Jansson <krustur@gmail.com>
2015-02-02 22:27:04 +0100

    Removed hard coded palette from rend.asm

--- src/platform/amiga/rend.asm
+++ src/platform/amiga/rend.asm
@@ -262,59 +262,23 @@ Copper_bplpt
 	dc.w	bplpt+10,$0000
 	dc.w	bplpt+12,$0000
 	dc.w	bplpt+14,$0000
+
 Copper_color
-	;dc.w	color+0,$0000	
-	;dc.w	color+2,$0EEE
-	;dc.w	color+4,$0CCC
-	;dc.w	color+6,$0888
-	;dc.w	color+8,$0666	
-	;dc.w	color+10,$0444
-	;dc.w	color+12,$0222
-	;dc.w	color+14,$0000
-	;dc.w	color+16,$00F0
-	;dc.w	color+18,$000F
-	;dc.w	color+20,$0FF0
-	;dc.w	color+22,$00FF
-	;dc.w	color+24,$0F0F
-	;dc.w	color+26,$0F80
-	;dc.w	color+28,$008F
-	;dc.w	color+30,$0F00
-
-	;dc.w	color+0, $0000	
-	;dc.w	color+2, $0000
-	;dc.w	color+4, $0444
-	;dc.w	color+6, $088a
-	;dc.w	color+8, $0eee
-	;dc.w	color+10,$004a
-	;dc.w	color+12,$008c
-	;dc.w	color+14,$0282
-	;dc.w	color+16,$02a2
-	;dc.w	color+18,$04c4
-	;dc.w	color+20,$0820
-	;dc.w	color+22,$0a60
-	;dc.w	color+24,$0e0e
-	;dc.w	color+26,$0e0e
-	;dc.w	color+28,$0e82
-	;dc.w	color+30,$0e04
-
 	dc.w	color+0, $0000	
 	dc.w	color+2, $0000
-	dc.w	color+4, $0444
-	dc.w	color+6, $0a88
-	dc.w	color+8, $0eee
-	dc.w	color+10,$0a04
-	dc.w	color+12,$0c08
-	dc.w	color+14,$0228
-	dc.w	color+16,$022a
-	dc.w	color+18,$044c
-	dc.w	color+20,$0082
-	dc.w	color+22,$00a6
-	dc.w	color+24,$0ee0
-	dc.w	color+26,$0ee0
-	dc.w	color+28,$02e8
-	dc.w	color+30,$04e0
-
+	dc.w	color+4, $0000
+	dc.w	color+6, $0000
+	dc.w	color+8, $0000
+	dc.w	color+10,$0000
+	dc.w	color+12,$0000
+	dc.w	color+14,$0000
+	dc.w	color+16,$0000
+	dc.w	color+18,$0000
+	dc.w	color+20,$0000
+	dc.w	color+22,$0000
+	dc.w	color+24,$0000
+	dc.w	color+26,$0000
+	dc.w	color+28,$0000
+	dc.w	color+30,$0000
+Copper_end
 	dc.w	$FFFF,$FFFE
-	;0e0e 0000 0444 0a88 0eee 0a40 0c80 0282
-	;02a2 04c4 0028 006a 0e0e 0e0e 028e 040e
-	
