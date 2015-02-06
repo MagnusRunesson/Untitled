64a06b1f2ed378c4a9b67c0b7bee7246e00b3635
Magnus Runesson <magnus@pokewhat.com>
2015-02-06 09:22:06 +0100

    Added cool sinus table

--- src/incasm/data.asm
+++ src/incasm/data.asm
@@ -1,4 +1,9 @@
+	; Various tables
+	include		"../src/incasm/sintable.asm"
+
+	; Images
 	include		"../src/incasm/testtiles_image.asm"
 	include		"../src/incasm/untitled_splash_image.asm"
 
+	; Game objects
 	include		"../src/incasm/gameobject_hero.asm"
