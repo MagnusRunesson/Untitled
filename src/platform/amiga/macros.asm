70599edbb36a8068dc41421dbc404f33875b1056
Krister Jansson <krustur@gmail.com>
2015-02-10 22:31:27 +0100

    Fixes of Amiga macros.asm

--- src/platform/amiga/macros.asm
+++ src/platform/amiga/macros.asm
@@ -1,19 +1,37 @@
-	; usage: 
+
+;==============================================================================
+;
+; Gets pointer to workmem (pc-relative), using workmembegin as a "middle label"
+;
+; usage: 
 ;  \1 = workmem symbol to get
 ;  \2 = where to place the ptr
 ; example:
-;  _get_workmem_ptr	BitplMem,a0 ; gets ptr to BitplaMem and places into register a0
+;  _get_workmem_ptr	BitplMem,a0 ; gets ptr to BitplaMem and places into 
+;  register a0
+;
+;==============================================================================
 _get_workmem_ptr	MACRO	\1 \2
 	lea			workmembegin(pc),\2
 	add.l		#(\1-workmembegin),\2
 	ENDM
 
-
+;==============================================================================
+;
+; Will set the border color to index 0 in the palette. Might get modified so 
+; the color index is a parameter.
+;
+;==============================================================================
 perf_start		MACRO
 	;move.w		#$0F00,$DFF180
 	ENDM
 
-
+;==============================================================================
+;
+; Will set the border color to index 1 in the palette. Might get modified so 
+; the color index is a parameter.
+;
+;==============================================================================
 perf_stop		MACRO
 	;move.w		#$0000,$DFF180
 	ENDM
