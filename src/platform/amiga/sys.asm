6def343db2c87ea58c28f34ae1cffbc3bffc928d
Krister Jansson <krustur@gmail.com>
2015-02-10 22:35:28 +0100

    Clean up/commenting of Amiga sys.asm

--- src/platform/amiga/sys.asm
+++ src/platform/amiga/sys.asm
@@ -1,22 +1,26 @@
+;==============================================================================
+;
+; Amiga entry point
+; Will orchestrate game system initialization and tear down, and restore of
+; Amiga-OS when runned from file
+;
+;==============================================================================
 entryPoint:
-	lea			_custom,a2
-	; move.w		#DMAF_ALL,dmacon(a2)
-	move.w		#DMAF_ALL|DMAF_MASTER,dmacon(a2)
 
+	; Initialization	
+	lea			_custom,a2
 	
-	
+	move.w		#DMAF_ALL|DMAF_MASTER,dmacon(a2)	
 	move.w		#(DMAF_SETCLR|DMAF_SPRITE|DMAF_BLITTER|DMAF_COPPER|DMAF_RASTER|DMAF_MASTER),dmacon(a2)
 	move.w		#$7FFF,intena(a5)
 	
-	
-	
 	bsr			rendInit
-
 	bsr			trackdiskInit
 
+	; Main loop
 	jsr			main(pc)
 	
-	; teardown
+	; Tear down, and restore of Amiga-OS
 	
 	rts
 
