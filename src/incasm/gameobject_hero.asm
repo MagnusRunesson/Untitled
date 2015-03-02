b5f1a488a6f5b1b46c4c011aad2cf62821ec60c8
Magnus Runesson <magnus@pokewhat.com>
2015-03-02 20:53:13 +0100

    Added stone block game object

--- src/incasm/gameobject_hero.asm
+++ src/incasm/gameobject_hero.asm
@@ -15,3 +15,9 @@ signpost_go:
 	dc.w		fileid_signpost_sprite
 	dc.w		8		; Hotspot X
 	dc.w		14		; Hotspot Y
+
+stoneblock_go:
+	dc.w		fileid_stoneblock_sprite_bank
+	dc.w		fileid_stoneblock_sprite
+	dc.w		8		; Hotspot X
+	dc.w		24		; Hotspot Y
