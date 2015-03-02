0d6c3e83ad0adf89a8e26eb33629527932075a82
Magnus Runesson <magnus@pokewhat.com>
2015-03-02 17:15:22 +0100

    Added signpost game object definition

--- src/incasm/gameobject_hero.asm
+++ src/incasm/gameobject_hero.asm
@@ -9,3 +9,9 @@ potion_go:
 	dc.w		fileid_testsprite2_sprite
 	dc.w		8		; Hotspot X
 	dc.w		8		; Hotspot Y
+
+signpost_go:
+	dc.w		fileid_signpost_sprite_bank
+	dc.w		fileid_signpost_sprite
+	dc.w		8		; Hotspot X
+	dc.w		14		; Hotspot Y
