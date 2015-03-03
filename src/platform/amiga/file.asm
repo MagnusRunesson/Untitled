43448fc61fbe9bd1850ccd112bc991831c9a885a
Krister Jansson <krustur@gmail.com>
2015-03-03 22:56:15 +0100

    Fixed bug in Amiga file.asm

--- src/platform/amiga/file.asm
+++ src/platform/amiga/file.asm
@@ -3,11 +3,11 @@
 ; Load file to specified memory address
 ;
 ; Input:
-;	d0=file ID
-;	a0=address to load file to
+;	d0.w=file ID
+;	a0.l=address to load file to
 ;
 ; Output
-;	d0=file size
+;	d0.l=file size
 ;
 ;==============================================================================
 
@@ -15,6 +15,7 @@ fileLoad
 	push.l		d2
 
 	lea			FileIDMap(pc),a1
+	ext.l		d0
 	asl.l		#2,d0
 	add.l		d0,a1
 	moveq		#0,d0
