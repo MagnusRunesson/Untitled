2722640d46340735341fd5519f7a79cafc6da782
Magnus Runesson <magnus@pokewhat.com>
2015-03-06 12:58:13 +0100

    When exporting a game object collection it will now also export an asm file with constants that can be used to refer to game objects within the collection.

--- util/BigTool/Assets/Editor/GameObjectCollection.cs
+++ util/BigTool/Assets/Editor/GameObjectCollection.cs
@@ -59,6 +59,12 @@ public class GameObjectCollection
 		int outsizePerObject = 2+2+2+2;
 		byte[] outBytes = new byte[ outsizePerObject * m_definitions.Count ];
 
+		string path = System.IO.Path.GetDirectoryName( _outPath );
+		string name = System.IO.Path.GetFileNameWithoutExtension( _outPath );
+		string asmOutputName = path + System.IO.Path.DirectorySeparatorChar + name + "_identifiers.asm";
+
+		string asmOutput = ";\n; Identifiers for the game objects in the '" + name + "' collection\n;\n";
+
 		int i;
 		for( i=0; i<m_definitions.Count; i++ )
 		{
@@ -68,14 +74,44 @@ public class GameObjectCollection
 			WriteShort( outBytes, wrofs+2, _project.GetIDFromConstant( def.m_spriteFileName ));
 			WriteShort( outBytes, wrofs+4, def.m_hotspotX );
 			WriteShort( outBytes, wrofs+6, def.m_hotspotY );
+
+			asmOutput += (def.m_identifier + "_gameobject").PadRight( 40 ) + " equ " + i + "\n";
 		}
 
 		System.IO.File.WriteAllBytes( _outPath, outBytes );
+
+		//
+		// Also export an asm file with all game object identifiers
+		//
+		System.IO.File.WriteAllText( asmOutputName, asmOutput );
 	}
 
 	void WriteShort( byte[] _outArray, int _byteOffset, int _value )
 	{
 		_outArray[ _byteOffset+0 ] = (byte)((_value>>8)&0xff);
 		_outArray[ _byteOffset+1 ] = (byte)(_value&0xff);
+	}
+
+	public Definition GetDefinitionFromIdentifier( string _identifier )
+	{
+		foreach( var def in m_definitions )
+		{
+			if( def.m_identifier.Equals( _identifier ))
+				return def;
+		}
+
+		return null;
+	}
+
+	public int GetIndexFromIdentifier( string _identifier )
+	{
+		int i;
+		for( i=0; i<m_definitions.Count; i++ )
+		{
+			if( m_definitions[ i ].m_identifier.Equals( _identifier ))
+				return i;
+		}
+
+		return -1;
 	}
 }
