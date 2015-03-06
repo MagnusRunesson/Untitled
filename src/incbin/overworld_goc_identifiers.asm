59b541e3f9c9a43ad570ae1e5420f9d5c0d31575
Magnus Runesson <magnus@pokewhat.com>
2015-03-06 12:58:56 +0100

    New export of data that also exported the game object definition identifiers into an asm file

---
+++ src/incbin/overworld_goc_identifiers.asm
@@ -0,0 +1,7 @@
+;
+; Identifiers for the game objects in the 'overworld_goc' collection
+;
+herotest_gameobject                      equ 0
+stoneblock_gameobject                    equ 1
+signpost_gameobject                      equ 2
+potion_gameobject                        equ 3
