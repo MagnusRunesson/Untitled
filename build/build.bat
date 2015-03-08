d261cc9333a74f483e292cf633f815af6d57a2ce
Krister Jansson <krustur@gmail.com>
2015-03-08 23:30:42 +0100

    Improved Build.bat console output. More spam to better show difference between Amiga and Mega Drive build output

--- build/build.bat
+++ build/build.bat
@@ -1,8 +1,20 @@
 @echo off
 
+Echo.
+Echo Amiga!
+Echo.
+
 ..\bin\win32\vasmm68k_mot_win32 -Fbin -o out/amigaadf.adf amigaadf.asm -Dis_amiga -m68000 -L amigaadf_listfile.txt -I"F:/Amiga/Harddrives/Hdf My Amiga 1200/NewCode/NDK_3.9/Include/include_i" -I"F:\Amiga\Harddrives\Hdf My Amiga 1200\NewCode\Source\Untitled\src\platform\amiga\amiga_lib"
 ..\bin\win32\makeimage out/amigaadf.adf
 ..\bin\win32\buildcomment amigaadf_listfile.txt mess\comments\a500.cmt a500
 
+Echo.
+Echo Mega drive!
+Echo.
+
 ..\bin\win32\vasmm68k_mot_win32 -Fbin -o out/untitled_megadrive.bin megadrive.asm -Dis_mega_drive -m68000 -L megadrive_listfile.txt 
 ..\bin\win32\buildcomment megadrive_listfile.txt mess\comments\genesis.cmt genesis
+
+Echo.
+Echo done..
+Echo.
