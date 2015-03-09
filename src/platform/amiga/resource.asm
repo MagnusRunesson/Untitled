12db309b36e39fb8065233bb1ab4a6afb2962a21
Krister Jansson <krustur@gmail.com>
2015-03-10 00:06:31 +0100

    Added resourceLoadGoc to Amiga

--- src/platform/amiga/resource.asm
+++ src/platform/amiga/resource.asm
@@ -105,3 +105,17 @@ resourceGetSprite
 	bsr			resourceGetBySlotIndex
 	rts
 
+;==============================================================================
+;
+; Wrapper for goc
+; Input
+;   d0.w=resource slot index
+; Output
+;   a0.l=pointer to resource
+;
+;==============================================================================
+
+resourceLoadGoc
+	bsr			resourceLoadStaticFile
+	rts
+
