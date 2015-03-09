990d2d22b09dedacb7bc5774f61dfe7bd74b3495
Krister Jansson <krustur@gmail.com>
2015-03-09 23:48:02 +0100

    Relocated include of mega drive incbin source files

--- build/megadrive.asm
+++ build/megadrive.asm
@@ -14,11 +14,11 @@ _chunk_size		equ		128			; same as TD_SECTOR (size of sector on floppy)
 	include		"../src/platform/megadrive/file.asm"
 	include		"../src/platform/megadrive/img.asm"
 	include		"../src/incasm/data.asm"
+	include		"../src/incbin/datasta_md.asm"
+	include		"../src/incbin/fileidmap_md.asm"
 
 	org			$10000
 
-	include		"../src/incbin/datasta_md.asm"
 	include		"../src/incbin/datadyn_md.asm"
 	include		"../src/incbin/files.asm"
-	include		"../src/incbin/fileidmap_md.asm"
 	include		"../src/incbin/overworld_goc_identifiers.asm"	; This should really be included by something else. Preferrably by data.asm
